4030a19ddc9b3b3faffcfb0aaebbb373617d313f
Scott Mielcarski <scott_mielcarski@me.com>
2018-08-15 18:48:27 -0700

    Improve output when printing columns

--- dotfiles/io_util.py
+++ dotfiles/io_util.py
@@ -7,14 +7,20 @@ def print_columns(data, padding=2):
             else:
                 column_widths[i] = max(column_widths[i], len(column))
     for row in data:
-        print("".join(column.ljust(column_widths[i] + padding) for (i, column) in enumerate(row)))
+        row_str = ""
+        for (i, column) in enumerate(row):
+            if i < (len(row) - 1):
+                row_str += column.ljust(column_widths[i] + padding)
+            else:
+                row_str += column
+        print(row_str)
 
 
 def boolean_input(message):
     response = None
     response_valid = False
     while not response_valid:
-        print(f'{message}, (y)es or (n)o?')
+        print(f"{message}, (y)es or (n)o?")
         response = input().lower()
         response_valid = (response == 'yes' or response == 'y' or response == 'no' or response == 'n')
     return response == 'yes' or response == 'y'
